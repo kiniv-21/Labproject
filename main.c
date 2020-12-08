74ec586010908ff0f55f5bdd060d6311c3b1467d
kiniv-21 <42902325+kiniv-21@users.noreply.github.com>
2020-12-08 11:12:58 +0530

    Update main.c

--- main.c
+++ main.c
@@ -15,15 +15,15 @@ int main()
 
     const int SIZE = 15;
     records rostr[SIZE];
-    rostr[0].id = 201046005;
-    rostr[0].name = "Abhish Ijari";
+    rostr[0].id = 201046036;
+    rostr[0].name = "Vivek V Kin";
     rostr[0].gpa = 9.8;
 
-    rostr[1].id = 201046006;
+    rostr[1].id = 201046406;
     rostr[1].name = "karthik G";
     rostr[1].gpa = 8.5;
 
-    rostr[2].id = 201046007;
+    rostr[2].id = 201046507;
     rostr[2].name = "Raghav Kumar";
     rostr[2].gpa = 7.5;
 
